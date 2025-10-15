86a4b3b27a7d5701fdbb4f66455f429b8a6f422b
Yurisha Bajracharya <yurishabajracharya@gmail.com>
2025-10-15 08:15:55 +0000

    update navbar style to look well for large screens too

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -93,7 +93,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center px-4 md:px-6 max-w-7xl">
+      <div className="container flex h-16 items-center px-4 md:px-6">
         <div className="flex items-center mr-auto">
           <div className="md:hidden mr-2">
             <Sheet>
